9e562bc67a0442765a15d2f00709f864088fa2f7
Iamnotsquishy <quinndibble@gmail.com>
2016-09-15 19:57:16 -0400

    added the reader() function

--- occupation.py
+++ occupation.py
@@ -1,26 +1,43 @@
 import random, csv
 # Main function
-def main():
+#def main():
 	
 
 
 # Reading function
 def reader():
-	jobdict = {}
-	with open('occupations.csv', 'rb') as occupations:
-		jobfinder = csv.reader(occupations, delimiter=',', quotechar='"')
-		for row in jobfinder: 
 
-	return
+	#open the file, and turn it into one big string
+	f = open('occupations.csv', 'r')
+	rawdata = f.read()
+
+	#split the string by the \n character, remove the heading and footing
+	joblist = rawdata.split("\r\n")
+	joblist.pop(0)
+	joblist.pop(-1)
+	joblist.pop(-1)
+
+	#turn each entry into a seperate list, remove " characters, convert the numbers to floats
+	for x in joblist:
+		if x[0] == '"':
+			x = x[1:len(x)]
+			x = x.split('",')
+		else:
+			x = x.split(',')
+		x[1] = float(x[1])
+
+	return joblist #returns the list of lists
+
 # Storage function
-def storer(dict):
+#def storer(list):
 
 
-	return
+#	return
 # Choosing function
-def chooser(storage):
+#def chooser(storage):
 
 
 
-	return
+#	return
 # Runtime
+print reader()
