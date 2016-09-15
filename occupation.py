2d26e71d59367d5e70bf5b2908a9c610b9d1f326
Iamnotsquishy <quinndibble@gmail.com>
2016-09-15 14:51:48 -0400

    edited occupation.py

--- occupation.py
+++ occupation.py
@@ -1,1 +1,26 @@
-print "Hello, World!"
+import random, csv
+# Main function
+def main():
+	
+
+
+# Reading function
+def reader():
+	jobdict = {}
+	with open('occupations.csv', 'rb') as occupations:
+		jobfinder = csv.reader(occupations, delimiter=',', quotechar='"')
+		for row in jobfinder: 
+
+	return
+# Storage function
+def storer(dict):
+
+
+	return
+# Choosing function
+def chooser(storage):
+
+
+
+	return
+# Runtime
